daad28047fd1571d55f65ee6c4fa539dd36cebc5
Kelsey Langlois and Safiy Cham <safiy@>
2017-08-22 09:14:50 -0700

    Add business logic to get price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 //business logic
+function Ticket (movie, showtime, age) {
+  this.movie = movie;
+  this.showtime = showtime;
+  this.age = age;
+}
 
+Ticket.prototype.getTicket = function () {
 
+  if (this.age >= 60) {
+    return 7;
+  } else if (this.showtime < 17) {
+    return 10;
+  } else {
+    return 12;
+  }
+}
 
 
 
@@ -12,11 +26,12 @@ $(document).ready(function() {
   $("form#movie-info").submit(function(event){
     event.preventDefault();
     var title = $("select#title").val();
-    var showTime = $("input#showtime").val();
-    var age = $("input#age").val();
+    var showTime = parseInt($("input#showtime").val());
+    var age = parseInt($("input#age").val());
 
-    console.log(title, showTime, age);
+    var customerTicket = new Ticket(title, showTime, age);
 
+    console.log(customerTicket.getTicket());
 
 
 
