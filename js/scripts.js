8e6a3412255fb1236ae857de1f7d8cc47a791802
Kelsey Langlois and Safiy Cham <kelsey@>
2017-08-22 13:43:36 -0700

    Add comments and formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 //business logic
+
+// Stores information we need about the ticket.
 function Ticket (movie, showtime, age) {
   this.movie = movie;
   this.showtime = showtime;
   this.age = age;
 }
 
+// Price depends on age and showtime, so this assigns the ticket price based on this Ticket object's values for age and showtime.
 Ticket.prototype.getTicket = function () {
   if (this.age >= 60) {
     return "$7";
@@ -15,6 +18,7 @@ Ticket.prototype.getTicket = function () {
   }
 }
 
+// We want our showtime to look like an actual time, so we change it out of 24-hr format and into AM/PM format
 Ticket.prototype.getShowtime = function() {
   if (this.showtime > 12) {
     return (this.showtime - 12) + " PM";
@@ -27,17 +31,23 @@ Ticket.prototype.getShowtime = function() {
 $(document).ready(function() {
   $("form#movie-info").submit(function(event){
     event.preventDefault();
+    // Get our movie information from the form: title, showTime, and customer age
     var title = $("select#title").val();
     var showTime = parseInt($("select#showtime").val());
     var age = parseInt($("input#age").val());
 
+    // Make a new ticket object using the input
     var customerTicket = new Ticket(title, showTime, age);
 
+    // Hide our form, show the ticket
     $(".result").slideToggle();
     $("form").slideToggle();
+
+    // Add information to the ticket section: Movie Title, Time, Price, and Poster
     $(".movieTitle").append(customerTicket.movie);
     $(".time").append(customerTicket.getShowtime());
     $(".ticketPrice").append(customerTicket.getTicket());
+    // This works because all poster images are named [Movie Title].jpeg
     $(".poster").append("<img src='img/" + customerTicket.movie + ".jpeg' alt='" + customerTicket.movie + "poster'>");
   });
 });
